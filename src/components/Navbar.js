857f9fa5dee9d0a7da2d217fd75a9f829ff09555
Omar Toumi <omartoumi158@gmail.com>
2024-04-24 12:39:37 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector,useDispatch } from "react-redux";
+import { logout } from "../redux/slices/userSlice";
+const Navbar = () => {
+  const { isAuth } = useSelector((state) => state.user);
+  const dispatch=useDispatch()
+  return (
+    <div>
+      {isAuth ? (
+        <>
+          <Link to="/profile">Profile</Link>
+          <button onClick={()=>{
+            dispatch(logout())
+          }}>Logout</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Register.js
@@ -0,0 +1,32 @@
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { UserRegister } from "../redux/slices/userSlice";
+const Register = () => {
+  const e = useRef();
+  const p = useRef();
+  const dispatch = useDispatch();
+  const { error, loading } = useSelector((state) => state.user);
+  return (
+    <div>
+      <input type="email" placeholder="Type your email" ref={e}></input>
+      <input type="password" placeholder="Type your password" ref={p}></input>
+      <button
+        onClick={() => {
+          dispatch(
+            UserRegister({
+              email: e.current.value,
+              password: p.current.value,
+            })
+          );
+        }}
+      >
+        Register
+      </button>
+      {loading && <h3>Loading....</h3>}
+
+      {error && <h3>{error}</h3>}
+    </div>
+  );
+};
+
+export default Register;
